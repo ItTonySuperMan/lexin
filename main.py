156705cbce2871a56495f172caa17f7b9668174a
ItTonySuperMan <35184901+ItTonySuperMan@users.noreply.github.com>
2021-01-14 10:33:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,113 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-import hashlib
 import time
-import datetime
-import sys
+import random
+# import gevent
+from hashlib import md5
+# from gevent import monkey
 
 
+class Step:
+    def __init__(self, users):
+        self.user_info = users
+        self.login_url = "https://sports.lifesense.com/sessions_service/login?" \
+                         "platform=android&systemType=2&version=4.6.7"
+        self.step_url = "https://sports.lifesense.com/sport_service/sport/sport/uploadMobileStepV2?" \
+                        "version=4.5&systemType=2"
+        self.user_agent = "Dalvik/2.1.0 (Linux; U; Android 9; SM-G9500 Build/PPR1.180610.011)"
+        self.band_ids = ["http://we.qq.com/d/AQC7PnaOelOaCg9Ux8c9Ew95yumTVfMcFuGCHMY-",
+                         "http://we.qq.com/d/AQC7PnaOi9BLVrfJIiVTU8ENIbv_9Lmlqia1ToGc",
+                         "http://we.qq.com/d/AQC7PnaOXQhy3VvzFeP5bZMKmAQrGE6NJWdK3Xnk",
+                         "http://we.qq.com/d/AQC7PnaOaEXBdhkdXQvTRE1CO1fIqBuitbSSGt2r",
+                         "http://we.qq.com/d/AQC7PnaOdI9h0tfCr0KRlb78ISAE9qcaZ3btHrJE",
+                         "http://we.qq.com/d/AQC7PnaOsThRYksmQcvpa0klKFrupqaqKyEPm8nj",
+                         "http://we.qq.com/d/AQC7PnaOk8V-FV7R4ix61GToC5fh5I151hvlsNf6",
+                        ]
+        self.bind_msg = ''
 
-class LexinSport:
-    def __init__(self, username, password, step):
-        self.username = username
-        self.password = password
-        self.step = step
 
-    # 登录
-    def login(self):
-        url = 'https://sports.lifesense.com/sessions_service/login?systemType=2&version=4.6.7'
-        data = {'loginName': self.username, 'password': hashlib.md5(self.password.encode('utf8')).hexdigest(),
-                'clientId': '49a41c9727ee49dda3b190dc907850cc', 'roleType': 0, 'appType': 6}
-        headers = {
-            'Content-Type': 'application/json; charset=utf-8',
-            'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 7.1.2; LIO-AN00 Build/LIO-AN00)'
-        }
-        response_result = requests.post(url, data=json.dumps(data), headers=headers)
-        status_code = response_result.status_code
-        response_text = response_result.text
-        # print('登录状态码：%s' % status_code)
-        # print('登录返回数据：%s' % response_text)
-        if status_code == 200:
-            response_text = json.loads(response_text)
-            user_id = response_text['data']['userId']
-            access_token = response_text['data']['accessToken']
-            return user_id, access_token
+    def get_id_token(self, phone, pwd):
+        header_login = {'Content-Type': 'application/json; charset=utf-8',
+                        'Accept-Encoding': 'gzip',
+                        'User-Agent': self.user_agent
+                        }
+        data_org_login = {"appType": 6, "clientId": md5(phone.encode(encoding="utf-8")).hexdigest(), "loginName": phone,
+                          "password": md5(pwd.encode(encoding="utf-8")).hexdigest(), "roleType": 0}
+        data_login = json.dumps(data_org_login)
+        resp_data = json.loads(requests.post(url=self.login_url, data=data_login, headers=header_login).text)
+        uid = resp_data['data']['userId']
+        token = resp_data['data']['accessToken']
+
+        return uid, token, phone
+
+
+    def bind_device(self, uid, token):
+        bind_url = "https://sports.lifesense.com/device_service/device_user/bind"
+        band_id = random.choice(self.band_ids)
+        bind_org_data = {
+                        "qrcode": band_id,
+                        "userId": int(uid)
+                        }
+        bind_data = json.dumps(bind_org_data)
+        bind_header = {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "Cookie": "accessToken=" + token,
+                    "User-Agent": self.user_agent
+                    }
+        bind_result = requests.post(url=bind_url, data=bind_data, headers=bind_header)
+        self.bind_msg = json.loads(bind_result.text)['msg']
+        time.sleep(5)
+
+
+    def set_step(self, uid, token, phone, step):
+        self.bind_device(uid, token)
+        header_step = {'Cookie': 'accessToken=' + token,
+                       'Content-Type': 'application/json; charset=utf-8'
+                       }
+        data_org_step = {"list":
+                             [{"DataSource": 2,
+                               "active": 1,
+                               "calories": str(int(step // 20)),
+                               "dataSource": 2,
+                               "deviceId": "M_NULL",
+                               "distance": int(step / 3),
+                               "exerciseTime": 0,
+                               "isUpload": 0,
+                               "measurementTime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
+                               "priority": 0,
+                               "step": step,
+                               "type": 2,
+                               "updated": float(str(time.time()).split(".")[0]) + float(str(time.time()).split(".")[1][:3]),
+                               "userId": uid}]
+                         }
+        data_step = json.dumps(data_org_step)
+        time.sleep(3)
+        result = json.loads(requests.post(url=self.step_url, data=data_step, headers=header_step).text)
+        data = list(set(result['data']['pedometerRecordHourlyList'][0]['step'].split(",")))
+
+        for i in range(len(data)):
+            data[i] = int(data[i])
+
+        if step > max(data):
+            print("手机号:", phone, " 当前步数:", step, " 绑定情况:", self.bind_msg)
         else:
-            return '登录失败。'
-
-    # 修改步数
-    def change_step(self):
-        # 登录结果
-        login_result = self.login()
-        if login_result == '登录失败':
-            return '登录失败'
-        else:
-            url = 'https://sports.lifesense.com/sport_service/sport/sport/uploadMobileStepV2?systemType=2&version=4.6.7'
-            data = {'list': [{'DataSource': 2, 'active': 1, 'calories': int(self.step/4), 'dataSource': 2,
-                              'deviceId': 'M_NULL', 'distance': int(self.step/3), 'exerciseTime': 0, 'isUpload': 0,
-                              'measurementTime': time.strftime('%Y-%m-%d %H:%M:%S'), 'priority': 0, 'step': self.step,
-                              'type': 2, 'updated': int(round(time.time() * 1000)), 'userId': login_result[0]}]}
-            headers = {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Cookie': 'accessToken=%s' % login_result[1]
-            }
-            response_result = requests.post(url, data=json.dumps(data), headers=headers)
-            status_code = response_result.status_code
-            # response_text = response_result.text
-            # print('修改步数状态码：%s' % status_code)
-            # print('修改步数返回数据：%s' % response_text)
-            if status_code == 200:
-                return '修改步数为【%s】成功' % self.step
-            else:
-                return '修改步数失败'
+            print("手机号:", phone, " 当前步数:", max(data), " 绑定情况:", self.bind_msg)
 
 
-# 睡眠到第二天执行修改步数的时间
-def get_sleep_time():
-    # 第二天日期
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    # 第二天7点时间戳
-    tomorrow_run_time = int(time.mktime(time.strptime(str(tomorrow), '%Y-%m-%d'))) + 25200
-    # print(tomorrow_run_time)
-    # 当前时间戳
-    current_time = int(time.time())
-    # print(current_time)
-    return tomorrow_run_time - current_time
+    def run(self):
+        for phone, pwd in self.user_info.items():
+            self.set_step(*(self.get_id_token(phone, pwd)), random.randint(7000, 15000))
+            time.sleep(2)
+            # jobs = [gevent.spawn(self.set_step, *(self.get_id_token(phone, pwd)), random.randint(7000, 15000))]
+            # gevent.joinall(jobs)
 
 
-if __name__ == "__main__":
-    # 最大运行出错次数
+if __name__ == '__main__':
+    # monkey.patch_all()
     username = sys.argv[1]
     password = sys.argv[2]
-    step = sys.argv[3]
-    try:
-        # 修改步数结果
-        result = LexinSport(str(username), str(password), int(step)).change_step()
-        print(result)
-    except Exception as e:
-        print('运行出错，原因：%s' % e)
+    user_info = {username: password,  # add your account here
+                 }
+    zqy = Step(user_info)
+    zqy.run()
